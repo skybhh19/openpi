5e37ac573861765b796a92043a93e62b303894d8
Tian Gao <tiangao@stanford.edu>
2026-06-06 16:56:45 -0700

    inference configs

--- train_sc.sh
+++ train_sc.sh
@@ -5,10 +5,10 @@
 #SBATCH --cpus-per-task=30 # Request 30 CPUs for this task
 #SBATCH --mem=256G
 #SBATCH --gres=gpu:1
-#SBATCH --job-name=pi05_droid_pen_cup_low_mem_finetune_0605 # Name the job (for easier monitoring)
+#SBATCH --job-name=pi05_droid_pen_cup_randompct50_low_mem_finetune_0605 # Name the job (for easier monitoring)
 #SBATCH --output=slurm-%j.out
 #SBATCH --account=iris
-#SBATCH --nodelist=iris-hgx-2
+#SBATCH --nodelist=iris-hgx-1
 #SBATCH --mail-user tiangao@stanford.edu
 #SBATCH --mail-type END,FAIL,REQUEUE,TIME_LIMIT_80
 
@@ -17,8 +17,8 @@ source .venv/bin/activate
 
 
 XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 \
-uv run --group rlds scripts/train.py pi05_droid_pen_cup_low_mem_finetune_0605 \
-  --exp-name=pi05_droid_pen_cup_low_mem_finetune_0605 \
+uv run --group rlds scripts/train.py pi05_droid_pen_cup_randompct50_low_mem_finetune_0605 \
+  --exp-name=pi05_droid_pen_cup_randompct50_low_mem_finetune_0605 \
   --overwrite
 
 # XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 uv run scripts/train.py pi0_fast_robomimic_square_random_post_left_close_low_low_mem_finetune_validation --exp-name=pi0_fast_robomimic_square_random_post_left_close_low_low_mem_finetune_validation --overwrite
